fc046c977d5511918f50a7ca2968d3b0d8a20a73
emh0009-create <emh0009@alu.medac.es>
2025-11-10 11:11:51 +0100

    Update data.php

--- app/data.php
+++ app/data.php
@@ -2,12 +2,12 @@
 $usuarios_bbdd = [
 'usuario1@taskflow.com' => [
 'id' => 1,
-'nombre' => 'Oskar Calvo',
+'nombre' => 'Enrique Moscoso',
 'password' => 'pass123' 
 ],
 'usuario2@taskflow.com' => [
 'id' => 2,
-'nombre' => 'Ana Alumna',
+'nombre' => 'Pedro',
 'password' => 'admin'
 ]
 ];
